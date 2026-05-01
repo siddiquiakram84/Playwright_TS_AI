8f73dc579ddf73354a2ae356adc435e903a7c301
Akram Siddiqui <siddiquiakram84@gmail.com>
2026-05-01 19:36:24 +0530

    feat(api): add new ingest and orchestrate routes with type checks
    - Introduced new API routes for ingest and orchestrate with TypeScript definitions.
    - Implemented runtime checks for HTTP methods (GET, HEAD, OPTIONS, POST, PUT, DELETE, PATCH) to ensure correct parameter types and return types.
    - Added utility types for better type safety and validation.
    - Included source maps for easier debugging during development.

---
+++ dashboard-next/.next/server/vendor-chunks/fs-constants.js
@@ -0,0 +1,24 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "vendor-chunks/fs-constants";
+exports.ids = ["vendor-chunks/fs-constants"];
+exports.modules = {
+
+/***/ "(rsc)/../node_modules/fs-constants/index.js":
+/*!*********************************************!*\
+  !*** ../node_modules/fs-constants/index.js ***!
+  \*********************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = (__webpack_require__(/*! fs */ \"fs\").constants) || __webpack_require__(/*! constants */ \"constants\")\n//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKHJzYykvLi4vbm9kZV9tb2R1bGVzL2ZzLWNvbnN0YW50cy9pbmRleC5qcyIsIm1hcHBpbmdzIjoiQUFBQSxpQkFBaUIsK0NBQXVCLElBQUksbUJBQU8sQ0FBQyw0QkFBVyIsInNvdXJjZXMiOlsiL2hvbWUvY2xzL3d3dy9QbGF5d3JpZ2h0X1RTX0FJL25vZGVfbW9kdWxlcy9mcy1jb25zdGFudHMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsibW9kdWxlLmV4cG9ydHMgPSByZXF1aXJlKCdmcycpLmNvbnN0YW50cyB8fCByZXF1aXJlKCdjb25zdGFudHMnKVxuIl0sIm5hbWVzIjpbXSwiaWdub3JlTGlzdCI6WzBdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(rsc)/../node_modules/fs-constants/index.js\n");
+
+/***/ })
+
+};
+;
